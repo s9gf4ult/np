b429032169cf0c03d7f3c05ae491644db6ea9f67
s9gf4ult <s9gf4ult@gmail.com>
2012-06-08 00:41:30 +0600

    modified:   Common.h 	modified:   Main.cpp

--- Common.h
+++ Common.h
@@ -32,34 +32,34 @@ int cmp(const T &a, const T &b) {
 }
 
 template <typename T>
-SimpleCandle *reduceCandleCollection(T *collection){
-  if (collection->size() > 0) {
-    AbstractCandle *a = collection->first();
-    double mx = a->getHighCost();
-    double mn = a->getLowCost();
-    double oc = a->getOpenCost();
-    double cc = a->getCloseCost();
+Candle reduceCandleCollection(T collection){
+  if (collection.size() > 0) {
+    Candle a = collection.first();
+    double mx = a.getHighCost();
+    double mn = a.getLowCost();
+    double oc = a.getOpenCost();
+    double cc = a.getCloseCost();
     double volume = 0;
-    QDateTime ot = a->getOpenTime();
-    QDateTime ct = a->getCloseTime();
+    QDateTime ot = a.getOpenTime();
+    QDateTime ct = a.getCloseTime();
     
     
-    typename T::const_iterator cnd = collection->begin();
-    for (; cnd != collection->end(); ++cnd) {
-      volume += (*cnd)->getVolume();
-      mx = qMax(mx, (*cnd)->getHighCost());
-      mn = qMin(mn, (*cnd)->getLowCost());
-      if ((*cnd)->getOpenTime() < ot) {
-        ot = (*cnd)->getOpenTime();
-        oc = (*cnd)->getOpenCost();
-      };
-      if ((*cnd)->getCloseTime() < ct) {
-        ct = (*cnd)->getCloseTime();
-        cc = (*cnd)->getCloseCost();
-      }
-    };
+    typename T::const_iterator cnd = collection.begin();
+    for (; cnd != collection.end(); ++cnd) {
+        volume += cnd->getVolume();
+        mx = qMax(mx, cnd->getHighCost());
+        mn = qMin(mn, cnd->getLowCost());
+        if (cnd->getOpenTime() < ot) {
+            ot = cnd->getOpenTime();
+            oc = cnd->getOpenCost();
+        };
+        if (cnd->getCloseTime() > ct) {
+            ct = cnd->getCloseTime();
+            cc = cnd->getCloseCost();
+        }
+    }
     
-    return new SimpleCandle(oc, cc, mn, mx, volume, ot, ct);
+    return Candle(new SimpleCandle(oc, cc, mn, mx, volume, ot, ct));
   };
   return 0;
 }
--- Main.cpp
+++ Main.cpp
@@ -9,40 +9,13 @@
 #include <QString>
 #include <QDateTime>
 
-class SetVolume : public CandleVisitor {
-public:
-    SetVolume(double v) : Volume(v) {}
-    virtual void visit(SimpleCandle *candle) {
-        candle->setVolume(Volume);
-    }
-
-    virtual void visit(SimpleTick *tick) {
-        tick->setVolume(Volume);
-    }
-
-protected:
-    double Volume;
-};
 
 int main () {
     QTextStream out(stdout);
-    out << sizeof(Candle) << "\n";
-    out.flush();
     out.setCodec(QTextCodec::codecForName("UTF-8"));
-    Candle x(new SimpleCandle());
-    Candle y(x);
-    out << x.toString() << "\n";
-    out.flush();
-    out << y.toString() << "\n";
-    out.flush();
-    out << "changing x\n";
-    out.flush();
-    SetVolume svt(20);
-    x.accept(&svt);
-    out.flush();
-    out << x.toString() << "\n";
-    out.flush();
-    out << y.toString() << "\n";
+
+
+
     out.flush();
 }
   
